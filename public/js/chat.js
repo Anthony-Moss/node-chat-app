aa53860974d349db458dc5968a64b3e8f5d9fcd5
Anthony Moss <asmoss93@gmail.com>
2019-09-02 18:57:42 -0400

    adds location template that handles what a location looks like in html, it is seperate event than regular message but still shows in messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,10 +10,17 @@ const $messages = document.querySelector('#messages')
 
 // templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector("#location-template").innerHTML
+
 
 socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, { message })
+    $messages.insertAdjacentHTML('afterbegin', html)
+})
+
+socket.on('locationMessage', (url) => {
+    const html = Mustache.render(locationTemplate, { url })
     $messages.insertAdjacentHTML('afterbegin', html)
 })
 
