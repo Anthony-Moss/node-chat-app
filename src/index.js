685a021da8a0785417249f8c4ee1245fc5a707fc
Anthony Moss <asmoss93@gmail.com>
2019-09-02 16:16:07 -0400

    adds button that sends coordinates of a users location to the chat uses geolocation api to get coords

--- src/index.js
+++ src/index.js
@@ -25,6 +25,10 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left!')
     })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `Location: ${coords.latitude}, ${coords.longitude}`)
+    })
 })
 
 server.listen(port, () => {
