f729b92bbeef0b912d3922bce0a10fbc728a9463
Anthony Moss <asmoss93@gmail.com>
2019-09-02 16:30:21 -0400

    adds bad-words npm module for filtering messages, currently not allowing profanity in sent messages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app)
@@ -18,8 +19,15 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!')
     socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('sendMessage', (msg) => {
+    socket.on('sendMessage', (msg, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(msg)) {
+            return callback('Profanity is not allowed!')
+        }
+
         io.emit('message', msg)
+        callback()
     })
 
     socket.on('disconnect', () => {
