282cb7dab3ac29fb30d520898b9157597dd825ac
Anthony Moss <asmoss93@gmail.com>
2019-09-03 18:35:17 -0400

    adds users js file that holds functions dealing with users, for now has add and remove user and getUser and getUsersInRoom

---
+++ src/utils/users.js
@@ -0,0 +1,78 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    // Clean data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser =  users.find((user) => {
+        return user.room === room && user.username === username && user.id === id
+    })
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is already in use!'
+        }
+    }
+
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return {
+        user
+    }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+
+}
+
+const getUser = (id) => {
+    // Check for user
+    const user = users.find((user) => user.id === id)
+
+    // Validate user
+    if (!user) {
+        return {
+            error: 'User not found'
+        }
+    }
+    // return user
+    return user
+}
+
+const getUsersInRoom = (room) => {
+    // check for users in room
+    const allUsers = users.filter((user) => user.room === room)
+
+    // Validate  user
+    if (!allUsers) {
+        return {
+            error: 'No users in this room'
+        }
+    }
+    // Return users
+    return allUsers
+}
+
+
+// Test calls
+
+
+// console.log(users)
