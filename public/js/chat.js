904073dd79919726fe927b44dc7a00a15fda1fbc
Anthony Moss <asmoss93@gmail.com>
2019-09-03 15:02:15 -0400

    adds timestamps to all messages sent

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,7 +15,10 @@ const locationTemplate = document.querySelector("#location-template").innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
-    const html = Mustache.render(messageTemplate, { message })
+    const html = Mustache.render(messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm:s A Z')
+    })
     $messages.insertAdjacentHTML('afterbegin', html)
 })
 
