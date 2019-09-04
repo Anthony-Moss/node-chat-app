4edfa116af686b5ecb3df84b0e7efc6c4cbe073f
Anthony Moss <asmoss93@gmail.com>
2019-09-03 23:38:25 -0400

    adds room title and users to the chat sidebar

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,7 +11,7 @@ const $messages = document.querySelector('#messages')
 // templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector("#location-template").innerHTML
-
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML
 
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
@@ -33,6 +33,14 @@ socket.on('locationMessage', (message) => {
         createdAt: moment(message.createdAt).format('h:mm:s A')
     })
     $messages.insertAdjacentHTML('beforebegin', html)
+})
+
+socket.on('roomData', ({ room, users}) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit', (e) => {
